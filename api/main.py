487e2a89d7161397036621090dc8e6b72068939b
Mario Stojcevski <mane.sto@outlook.com>
2026-02-07 19:28:04 +0100

    help

--- api/main.py
+++ api/main.py
@@ -108,4 +108,5 @@ def delete_game(game_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", "4444"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
